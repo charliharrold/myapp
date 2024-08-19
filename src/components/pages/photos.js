1f789da77b8a238e08876725ec0d61b43ae16c6f
Charli <charliharrold@gmail.com>
2024-08-19 14:41:10 -0700

    The photos page spacing is perfect! Wonderful!

--- src/components/pages/photos.js
+++ src/components/pages/photos.js
@@ -57,31 +57,33 @@ const alignAndLeft = {
 const Projects = () => {
     return (
         <div>
-
+            
             <h1>Welcome to my photos</h1>
             <p>I recently went on a trip, here are some pics from it.</p>
 
             <div>
-                <h3 style={rightAlign}>Rosetta Stone</h3>
-                <p style={rightAlign}>
-                    Writing next to rosetta stone
-                </p>
-                <img src='image1.jpg' width='307' height='408'/>
+            <img style={leftAlign} src='image1.jpg' width='307' height='408'/>
+                <div style={alignAndRight}>
+                    <h3>Rosetta Stone</h3>
+                    <p>Writing next to rosetta stone keep on writing to see what happens</p>
+                </div>
             </div>
 
             <div>
             <img style={rightAlign} src='image2.jpg' width='408' height='307'/>
-                <h3 style={alignOne}>London picture</h3>
-                <p style={alignOne}>
-                    Writing next to London picture of me
-                </p>
+
+                <div style={alignAndLeft}>
+                    <h3>London picture</h3>
+                    <p>Writing next to London picture of me</p>
+                </div>
             </div>
 
             <div>
-            <img style={leftAlign} src='image3.jpg' width='408' height='307'/>
+                <img style={leftAlign} src='image3.jpg' width='408' height='307'/>
+
                 <div style={alignAndRight}>
-                <h3>Thistle Picture</h3>
-                <p>Writing next to thistle picture. what happens if i keep writing</p>
+                    <h3>Thistle Picture</h3>
+                    <p>Writing next to thistle picture. what happens if i keep writing</p>
                 </div>
             </div>
 
@@ -89,11 +91,11 @@ const Projects = () => {
                 <img style={rightAlign} src='image4.jpg' width='408' height='307'/>
                 
                 <div style={alignAndLeft}>
-                <h3>Me in Skye</h3>
-
-                <p>The picture of me in Skye when it was windy.</p>
+                    <h3>Me in Skye</h3>
+                    <p>The picture of me in Skye when it was windy.</p>
                 </div>
             </div>
+
         </div>
     );
 };
