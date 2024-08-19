74abc8bd3bc8d07172c1641e41000b73449fdd2c
Charli <charliharrold@gmail.com>
2024-08-19 14:23:24 -0700

    Photos section is formatted 99% correctly!

--- src/components/pages/photos.js
+++ src/components/pages/photos.js
@@ -1,4 +1,3 @@
-import { clear } from "@testing-library/user-event/dist/clear";
 import React from "react";
 
 const rightAlign = {
@@ -45,44 +44,55 @@ const leftFloat = {
 
 const alignAndRight = {
     float: 'right',
-    clear: 'both',
-    display: 'flex',
+    display: 'grid',
+    alignItems: 'center',
+}
+
+const alignAndLeft = {
+    float: 'left',
+    display: 'grid',
     alignItems: 'center',
 }
 
 const Projects = () => {
     return (
         <div>
+
             <h1>Welcome to my photos</h1>
+            <p>I recently went on a trip, here are some pics from it.</p>
 
             <div>
                 <h3 style={rightAlign}>Rosetta Stone</h3>
-                
                 <p style={rightAlign}>
                     Writing next to rosetta stone
                 </p>
                 <img src='image1.jpg' width='307' height='408'/>
             </div>
-      
 
             <div>
             <img style={rightAlign} src='image2.jpg' width='408' height='307'/>
                 <h3 style={alignOne}>London picture</h3>
-                
                 <p style={alignOne}>
                     Writing next to London picture of me
                 </p>
-                
             </div>
 
             <div>
             <img style={leftAlign} src='image3.jpg' width='408' height='307'/>
-                <h3 style={alignAndRight}>Thistle Picture</h3>
-                <p style={alignAndRight}>Writing next to thistle picture.</p>
-                
+                <div style={alignAndRight}>
+                <h3>Thistle Picture</h3>
+                <p>Writing next to thistle picture. what happens if i keep writing</p>
+                </div>
             </div>
+
             <div>
                 <img style={rightAlign} src='image4.jpg' width='408' height='307'/>
+                
+                <div style={alignAndLeft}>
+                <h3>Me in Skye</h3>
+
+                <p>The picture of me in Skye when it was windy.</p>
+                </div>
             </div>
         </div>
     );
