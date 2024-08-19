fa1c9c3e06cf9f2427b2c4270beecd00d40aa66e
Charli <charliharrold@gmail.com>
2024-08-19 15:29:05 -0700

    I can't get text wrapping to work properly with the images. Using a break works fine for now.

--- src/components/pages/photos.js
+++ src/components/pages/photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdWrapText } from "react-icons/md";
 
 const rightAlign = {
     float: 'right',
@@ -18,26 +19,6 @@ const alignOne = {
 }
 
 
-
-
-
-/*
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-*/
-
 const leftFloat = {
     float: 'left',
 }
@@ -52,12 +33,13 @@ const alignAndLeft = {
     float: 'left',
     display: 'grid',
     alignItems: 'center',
+    //'overflow-wrap': 'break-word',
 }
 
 const Projects = () => {
     return (
         <div>
-            
+
             <h1>Welcome to my photos</h1>
             <p>I recently went on a trip, here are some pics from it.</p>
 
@@ -75,6 +57,24 @@ const Projects = () => {
                 <div style={alignAndLeft}>
                     <h3>London picture</h3>
                     <p>Writing next to London picture of me</p>
+                </div>
+            </div>
+
+            <div>
+                <img style={leftAlign} src='image3.jpg' width='408' height='307'/>
+
+                <div style={alignAndRight}>
+                    <h3>Thistle Picture</h3>
+                    <p>Writing next to thistle picture. what happens if i keep writing</p>
+                </div>
+            </div>
+
+            <div>
+                <img style={rightAlign} src='image4.jpg' width='408' height='307'/>
+                
+                <div style={alignAndLeft}>
+                    <h3>Me in Skye</h3>
+                    <p>The picture of me in Skye when it was windy.</p>
                 </div>
             </div>
 
