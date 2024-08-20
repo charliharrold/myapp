477c8d84d7ad09d703dde33c8a3282bc70880291
Charli <charliharrold@gmail.com>
2024-08-19 18:26:10 -0700

    Added some information to the resources tab.

--- src/components/pages/resources.js
+++ src/components/pages/resources.js
@@ -4,15 +4,20 @@ import React from "react";
 
 const Resources = () => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "centre",
-                alignItems: "centre",
-                height: "100vh",
-            }}
-        >
-            <h1>Welcome to my resources</h1>
+        <div>
+            <h1>Here you will find resources that I've made for various things</h1>
+
+            <h3>Introductory Programming</h3>
+            <p>
+                Here are the practice questions I made for a review session for an Introduction to Programming course
+                taught in Python at UVic. These materials were made by me on behalf of the Women in Engineering and Computer
+                Science (WECS) club, who can be found <a href="https://uvicwecsofficial.wixsite.com/wecsuvic">here</a>.
+            </p>
+
+            <h3>Make Your Own Website</h3>
+            <p>
+                I will soon be uploading a step-by-step guide on how to make your own website similar to this one!
+            </p>
         </div>
     );
 };
