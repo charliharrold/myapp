dffe6a355b767f0802d860aa0d2784ee3c41ebdf
Charli <charliharrold@gmail.com>
2024-08-19 18:11:27 -0700

    Added information to the projects page. Need to actually add the projects in.

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,16 +1,46 @@
 import React from "react";
 
+
 const Projects = () => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "centre",
-                alignItems: "centre",
-                height: "100vh",
-            }}
-        >
-            <h1>Welcome to my projects</h1>
+        <div>
+
+            <h1>Welcome to my projects page!</h1>
+
+            <h3>Website</h3>
+            <p>
+                I made this website myself (with lots of help from the internet of course) using JavaScript, CSS, and HTML.
+                Prior to making this website I had no real experience with any of these languages, but I had heard about React and decided that it
+                was something worth learning. So, I threw myself into the deep end! It was quite difficult to get started since the development process
+                felt quite different to what I was used to in other projects I've worked on but it was a valuable learning experience.
+                I imagine that the code I wrote isn't the best, but this is an ongoing project, and as I learn more I will go back and update
+                things to be better! But, for a first attempt, I'm pretty proud of it!
+            </p>
+
+            <h3>Napier Commission Analysis</h3>
+            <p>
+                In third year I took a class about Scottish history which was an incredible learning experience, especially since I was born and grew up in Scotland.
+                The Napier Commission was set up as a response to the unrest in the Highlands of Scotland due to... a lot of things, but
+                the core of the issue was the Highland Clearances. The Napier Commission stood out to me because it is one of the only times
+                when we hear from the Gaelic people themselves. After attending the 2024 Digital Humanities Summer Institute course
+                on natural language processing, I realized that my programming skills could be used in a humanities context.
+                So, I decided that I would do some analysis of the Napier Commission to determine the main concerns of the people.
+                We already know that the issues were lack of food, expensive rents, and the stealing of land, but looking at the quantitative analysis
+                of the Napier Commission shows just how much the people cared about these things.
+                <br></br>
+                I want to continue this project in an attempt to analyze how men and women interacted differently with their
+                environment.
+            </p>
+
+            <h3>Ocean Networks Canada API</h3>
+            <p>
+                I participated in the 2024 Digital Humanities Summer Institute course about APIs in Python, and used ONC's API as
+                a case study.
+                I'm so glad that I decided to attend this course because the idea of APIs was really intimidating to me before.
+                Getting into the ONC API was really hard! It is a wonderful service, but it's really hard to start with because there are
+                so many possibilities. I managed to get some cool graphs out of it, but it did prove to be quite the process.
+            </p>
+
         </div>
     );
 };
