6dc950d6eff415995a64236f161ca6ca84f82a83
John Lennox <jl1998@hotmail.co.uk>
2024-08-23 22:59:32 +0100

    Nav bar fixes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,21 +1,27 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const makePink = {
     "background-color": "#fec5e5",
 }
 
 function Navigation() {
+  const [activeKey, setActiveKey] = useState('#home');
+
+  const handleBrandClick = () => {
+    setActiveKey('#home');
+  };
+
   return (
     <Navbar expand="lg" style={makePink}>
         
       <Container>
-        <Navbar.Brand href="#home">Charli Harrold</Navbar.Brand>
+        <Navbar.Brand href="#home"  onClick={handleBrandClick}>Charli Harrold</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
